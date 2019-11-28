c2c3a2e118cbfcfcdb5e61cc538b1cd8b21185cb
DmitryDanilov <d.danilov@vironit.co.uk>
2019-11-28 18:13:56 +0300

    >settingsfragment

--- app/src/main/java/com/example/bottomnavwithnavigationcomponents/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/bottomnavwithnavigationcomponents/settings/SettingsFragment.kt
@@ -2,7 +2,9 @@ package com.example.bottomnavwithnavigationcomponents.settings
 
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.bottomnavwithnavigationcomponents.R
@@ -10,6 +12,17 @@ import kotlinx.android.synthetic.main.fragment_settings.*
 
 
 class SettingsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
